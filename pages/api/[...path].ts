12ec4a3e82ef63d2d79b679b8c0636f56c084d14
L15C <lehuuloc126@gmail.com>
2022-03-22 17:43:13 +0700

    handle login / logout requests

--- pages/api/[...path].ts
+++ pages/api/[...path].ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
 import httpProxy from 'http-proxy';
+import Cookies from 'cookies';
 
 // type Data = {
 //   name: string;
@@ -15,12 +16,25 @@ export const config = {
 const proxy = httpProxy.createProxyServer();
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-  // don't send cookies to API server
-  req.headers.cookie = '';
+  return new Promise((resolve) => {
+    // convert cookies to header Authorization
+    const cookies = new Cookies(req, res);
+    const accessToken = cookies.get('access_token');
+    if (accessToken) {
+      req.headers.Authorization = `Bearer ${accessToken}`;
+    }
+    // don't send cookies to API server
+    req.headers.cookie = '';
 
-  proxy.web(req, res, {
-    target: process.env.API_URL,
-    changeOrigin: true,
-    selfHandleResponse: false,
+    //
+    proxy.web(req, res, {
+      target: process.env.API_URL,
+      changeOrigin: true,
+      selfHandleResponse: false,
+    });
+
+    proxy.once('proxyRes', () => {
+      resolve(true);
+    });
   });
 }
