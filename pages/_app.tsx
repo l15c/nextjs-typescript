e445cdff27214163324dbb730e2f6c9cdd4b17bc
L15C <lehuuloc126@gmail.com>
2022-03-23 11:05:54 +0700

    Setup MUI v5

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,35 @@
 import axiosClient from '@/api-client/axios-client';
 import { EmptyLayout } from '@/components/layout';
 import { AppPropsWithLayout } from '@/models';
-import axios from 'axios';
+import { createEmotionCache, theme } from '@/utils';
+import { CacheProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 import { SWRConfig } from 'swr';
 import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+// Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache();
+
+function MyApp({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}: AppPropsWithLayout) {
   const Layout = Component.Layout ?? EmptyLayout;
 
   return (
-    <SWRConfig
-      value={{
-        fetcher: (url) => axiosClient.get(url),
-        shouldRetryOnError: false,
-      }}
-    >
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </SWRConfig>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <SWRConfig value={{ fetcher: (url) => axiosClient.get(url), shouldRetryOnError: false }}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SWRConfig>
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
 
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,10 +1,17 @@
 import { AdminLayout } from '@/components/layout';
+import { Box, Typography } from '@mui/material';
 import React from 'react';
 
 export interface AboutPageProps {}
 
 export default function AboutPage(props: AboutPageProps) {
-  return <div>About page</div>;
+  return (
+    <Box>
+      <Typography component="h1" variant="h3" color="primary.main">
+        About page
+      </Typography>
+    </Box>
+  );
 }
 
 AboutPage.Layout = AdminLayout;
