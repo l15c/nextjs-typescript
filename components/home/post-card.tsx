a849aa5b67f38a41e2e2a33b5639fdac14dee03f
L15C <lehuuloc126@gmail.com>
2022-05-16 16:31:10 +0700

    Post card

--- components/home/post-card.tsx
+++ components/home/post-card.tsx
@@ -1,7 +1,7 @@
 import { Post } from '@/models';
-import { Card, CardContent, Divider, Stack, Typography } from '@mui/material';
-import * as React from 'react';
+import { Card, CardContent, Divider, Typography } from '@mui/material';
 import { format } from 'date-fns';
+import * as React from 'react';
 export interface PostCardProps {
   post: Post;
 }
@@ -14,13 +14,11 @@ export function PostCard({ post }: PostCardProps) {
         <Typography variant="h5" fontWeight="bold">
           {post.title}
         </Typography>
-
-        <Typography variant="body1" display="flex" my={2}>
+        <Typography component="span" variant="body1" display="flex" my={2}>
           {format(Number(post.publishedDate), 'dd MMM yyyy')}
           <Divider orientation="vertical" flexItem sx={{ borderColor: '#999', mx: 3 }} />
           {post.tagList.join(', ')}
         </Typography>
-
         <Typography variant="body2">{post.description}</Typography>
       </CardContent>
     </Card>
