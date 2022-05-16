c1f5964fffe3db9e32e03779824d43412b209dd8
L15C <lehuuloc126@gmail.com>
2022-05-16 16:31:01 +0700

    Footer

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,25 +1,27 @@
 import { Facebook, Instagram, LinkedIn, Twitter } from '@mui/icons-material';
-import { Stack, Typography } from '@mui/material';
+import { Box, Icon, Stack, Typography } from '@mui/material';
 import * as React from 'react';
 
 export interface FooterProps {}
 
 export function Footer(props: FooterProps) {
+  const socialLinks = [
+    { icon: Facebook, url: 'http://google.com' },
+    { icon: Instagram, url: 'http://google.com' },
+    { icon: Twitter, url: 'http://google.com' },
+    { icon: LinkedIn, url: 'http://google.com' },
+  ];
+
   return (
-    <Stack
-      justifyContent="center"
-      direction="column"
-      component="footer"
-      height="182px"
-      textAlign="center"
-    >
+    <Box component="footer" py={2} textAlign="center">
       <Stack spacing={2} direction="row" justifyContent="center" pb={3}>
-        <Facebook />
-        <Instagram />
-        <Twitter />
-        <LinkedIn />
+        {socialLinks.map((item, idx) => (
+          <Box key={idx} component="a" href={item.url} target="_blank" rel="noopener noreferrer">
+            <Icon component={item.icon} sx={{ fontSize: 48 }} />
+          </Box>
+        ))}
       </Stack>
-      <Typography>Copyright ©2020 All rights reserved </Typography>
-    </Stack>
+      <Typography>Copyright ©{new Date().getFullYear()} All rights reserved </Typography>
+    </Box>
   );
 }
