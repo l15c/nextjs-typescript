8d5e35efa4b13c7fafa74220c547e6ed403513dd
L15C <lehuuloc126@gmail.com>
2022-05-16 16:31:19 +0700

    SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Seo } from '@/components/common';
 import { FeaturedWorks, HeroSection, RecentPosts } from '@/components/home';
 import { MainLayout } from '@/components/layout';
 import { NextPageWithLayout } from '@/models';
@@ -6,6 +7,17 @@ import { Box } from '@mui/material';
 const Home: NextPageWithLayout = () => {
   return (
     <Box>
+      <Seo
+        data={{
+          title: 'NextJS Tutorials | L15C',
+          description:
+            'Step by step tutorials to build a full CRUD website using NextJS for beginners',
+          url: 'https://nextjs-typescript-iota.vercel.app/',
+          thumbnailUrl:
+            'https://cdn.getshifter.co/caa65008efb706a8bfc6f7e4045d6a018420c3df/uploads/2020/11/nextjs-1200x733.png',
+        }}
+      />
+
       <HeroSection />
       <RecentPosts />
       <FeaturedWorks />
