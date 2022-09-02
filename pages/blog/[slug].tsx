8887b8dd072da2bd01796404e99cb0f837ac3782
L15C <lehuuloc126@gmail.com>
2022-09-03 00:02:51 +0700

    Markdown to HTML (Pakage + test)

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,10 +1,16 @@
 import { Post } from '@/models';
 import { getPostList } from '@/utils/posts';
-import { Container } from '@mui/material';
+import { Container, Divider } from '@mui/material';
 import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next';
 import { route } from 'next/dist/server/router';
 import { useRouter } from 'next/router';
 import * as React from 'react';
+import rehypeDocument from 'rehype-document';
+import rehypeFormat from 'rehype-format';
+import rehypeStringify from 'rehype-stringify';
+import remarkParse from 'remark-parse/lib';
+import remarkRehype from 'remark-rehype';
+import { unified } from 'unified';
 
 export interface BlogPageProps {
   post: Post;
@@ -19,8 +25,11 @@ export default function PostDetailPage({ post }: BlogPageProps) {
       <p>{post.title}</p>
       <p>{post.author?.name}</p>
       <p>{post.description}</p>
-
       <p>{post.mdContent}</p>
+
+      <Divider />
+
+      <div dangerouslySetInnerHTML={{ __html: post.htmlContent ?? '' }}></div>
     </Container>
   );
 }
@@ -45,6 +54,15 @@ export const getStaticProps: GetStaticProps<BlogPageProps> = async (
   const post = postList.find((x) => x.slug === slug);
   if (!post) return { notFound: true };
 
+  // Parse md to HTML
+  const file = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeDocument, { title: 'Blog detail page' })
+    .use(rehypeFormat)
+    .use(rehypeStringify)
+    .process(post.mdContent ?? '');
+  post.htmlContent = file.toString();
   return {
     props: {
       post,
